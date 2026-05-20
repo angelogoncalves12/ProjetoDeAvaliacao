099377ac6093ddcdfeae910e4ee09541b83cd45c
Angelo Clangs <contatoproangelo@gmail.com>
2026-05-20 20:27:04 -0300

    CORREÇÃO DE TOTAL

--- ControleDeAprovacao.C
+++ ControleDeAprovacao.C
@@ -76,7 +76,6 @@ int main() {
 
     return 0;
 }*/
-
 #include <stdio.h>
 int main() {
     float notas[8];
@@ -94,10 +93,6 @@ int main() {
 
         printf ("Insira a Frequência do Aluno: ");
         scanf ("%f", &frequencia);
-
-        if (notas[i] > maiorNota){
-            maiorNota = notas[i];
-        }
         
          if (notas[i] >=7 && frequencia >=75) {
             printf ("Aluno Aprovado\n");
@@ -130,9 +125,15 @@ int main() {
         }
 */
     }
+ 
+    for (i=1; i<=8; i++) {
+        if (notas[i] >= maiorNota){
+            maiorNota = notas[i];
+        }
+    }
+        
         printf ("O número de aprovados foi: %d\n", aprovados);
         printf ("O número de reprovados foi: %d\n", reprovados);
         printf ("A maior nota registrada: %.2f\n", maiorNota);
    
-    return 0;
-}
+    return 0; }
